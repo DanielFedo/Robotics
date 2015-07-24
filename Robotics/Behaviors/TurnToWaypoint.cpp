507c2fc55b2da45ad06aa48204440e53bb969d70
colman <colman@ubuntu>
2015-07-24 09:27:33 -0700

    Finished TurnToWayPoint

--- Robotics/Behaviors/TurnToWaypoint.cpp
+++ Robotics/Behaviors/TurnToWaypoint.cpp
@@ -12,21 +12,40 @@ TurnToWaypoint::TurnToWaypoint(Robot *robot) : Behaviors(robot)
 
 bool TurnToWaypoint::startCond()
 {
-	return !canGoFroward();
+	// Turn to waypoint if we have eye contact to it
+		Point* currWayPoint = WaypointsManager::getInstance()->getCurrWayPoint();
+
+		// Check if it's the last waypoint
+		if (currWayPoint == NULL){
+			return false;
+		}
+
+		if (currWayPoint->x - robot->getXPos() == 0){
+			nextWaypointYaw = robot->getYPos() - currWayPoint->y ? 180 : 0;
+		}
+		else{
+			double arctan = atan((currWayPoint->y - robot->getYPos()) / (currWayPoint->x - robot->getXPos()));
+			nextWaypointYaw = 180 -
+				Utils::RadianToDegree(arctan);
+		}
+
+		isLeftTurn = robot->getYaw() - nextWaypointYaw < 0 ? 1 : -1;
+
+		return true;
 }
 
 bool TurnToWaypoint::stopCond()
 {
-	if (canGoFroward() == false)
-	{
-		robot->setSpeed(0.0,0.0);
-		return true;
-	}
-
-	return false;
+	// stop if we reached way point yaw
+	return fabs(nextWaypointYaw - robot->getYaw()) < 2;
 }
 
 void TurnToWaypoint::action()
 {
-	robot->setSpeed(0.0, 1.0);
+	robot->setSpeed(0.0, isLeftTurn*0.2);
+}
+
+double TurnToWaypoint::getPriority()
+{
+	return 99999.0;
 }
--- Robotics/Behaviors/TurnToWaypoint.h
+++ Robotics/Behaviors/TurnToWaypoint.h
@@ -9,14 +9,22 @@
 #define TURNTOWAYPOINT_H_
 
 #include "Behaviors.h"
+#include "WaypointsManager.h"
+#include "AStar/Point.h"
+#include "Robot.h"
+#include <cmath>
 
 class TurnToWaypoint: public Behaviors {
+	double nextWaypointYaw;
+	int isLeftTurn;
 public:
 	TurnToWaypoint(Robot *robot);
 
 	bool startCond();
 	bool stopCond();
 	void action();
+	double getPriority();
+
 };
 
 #endif /* TURNTOWAYPOINT_H_ */
